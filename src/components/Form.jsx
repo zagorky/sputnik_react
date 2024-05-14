c70120075c710dabf3207238bdfed489b6520d91
zagorky <zagorkiynahyi@gmail.com>
2024-05-14 18:47:19 +0300

    заготовки для реакта

---
+++ src/components/Form.jsx
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+
+// <section class="subscribe">
+// <form class="subscribe-form">
+//   <div class="subscribe-form-header">
+//     <h3 class="subscribe-form-header-title">Подпишись!</h3>
+//     <div class="subscribe-form-header-subtitle">Информация о жизни бренда и новых коллекциях</div>
+//   </div>
+//   <div class="subscribe-form-body">
+//     <label for="user-email" class="visually-hidden">Email</label>
+//     <input class="subscribe-form-input" id="user-email" placeholder="Ваш email" />
+//     <button class="subscribe-form-button" type="submit">Отправить</button>
+//   </div>
+// </form>
+// </section>
---
+++ src/components/Swiper.jsx
@@ -0,0 +1,60 @@
+
+
+
+
+
+
+
+// <section>
+// <div class="swiper-container">
+//   <div class="swiper-wrapper">
+//     <div class="swiper-slide"><div class="container">
+//       <div class="main">
+                  
+//         <div class="text">
+//           <div><h2>Новый взгляд</h2></div>
+//           <div><p>на привычные вещи</p></div>
+//           <div class="butn"><button class="text-button"><a href="#catalogId">Посмотреть</a></button></div>
+//         </div>
+//         <div class="image">
+//           <div><img src="img/photo-1.jpg"></div>
+//         </div>
+//       </div>
+//     </div>      </div>
+//     <div class="swiper-slide"><div class="container">
+//       <div class="main">
+              
+//         <div class="text">
+//           <div><h2>Желтый цвет</h2></div>
+//           <div><p>всегда в моде</p></div>
+//           <div><button class="text-button"><a href="#catalogId">Подробнее</a></button></div>
+//         </div>
+//         <div class="image">
+//           <div><img src="img/photo-2.jpg"></div>
+//         </div>
+            
+               
+//       </div>
+//     </div>      </div>
+//     <div class="swiper-slide"><div class="container">
+//       <div class="main">
+                
+//         <div class="text">
+//           <div><h2>Скидка 25%</h2></div>
+//           <div><p>на летнюю коллекцию</p></div>
+//           <div><button class="text-button"><a href="#catalogId">Подробнее</a></button></div>
+//         </div>
+//         <div class="image">
+//           <div><img src="img/photo-3.jpg"></div>
+//         </div>
+//       </div>
+              
+                      
+//     </div></div>
+    
+//   </div>
+  
+//   <div class="swiper-pagination"></div>
+//   </div>
+// </div>
+// </section>
